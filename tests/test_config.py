c75e2d1d809e9d370d967122dca2b620cf057442
Daniel Capecci <7775585+dk0d@users.noreply.github.com>
2024-03-05 21:13:26 -0500

    fix: update tests (still need to improve alot)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from torchinstaller.utils import getCommandForPlatform, loadConfig, availableCudaVersions
+from torchinstaller.utils import getCommandForPlatform, getCudaVersion, loadConfig, availableCudaVersions
 from torchinstaller._soup import get_latest_version
 from torchinstaller._parse import sync_commands
 
@@ -21,13 +21,14 @@ def test_get_latest():
     assert version is not None
 
 
-def test_derive_latest():
+def test_build_commands():
     config = loadConfig(Path("./torchinstaller/config/commands.toml"))
     for command_key in ["pip", "conda", "mamba"]:
-        print("-" * 80)
         print(command_key)
         for platform in availableCudaVersions(config):
-            print(platform)
-            # print("-" * 80
-        # command = getCommandForPlatform(config, command_key, "latest", platform)
-        # print(command)
+            try:
+                print(platform)
+                command = getCommandForPlatform(config, command_key, "latest", platform)
+                print(command)
+            except Exception as e:
+                print(f"{platform} Error: ", e)
